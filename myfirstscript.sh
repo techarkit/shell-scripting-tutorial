2f3a2773fbc8ed269eb38be4a8c2b785393a9196
Ankam Ravi Kumar <techtutorials@users.noreply.github.com>
2018-05-02 17:10:34 +0530

    Create myfirstscript.sh

---
+++ myfirstscript.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Purpose: This is my first script in this shell scripting video tutorial
+#Date: Wed May  2 17:10:13 IST 2018
+#Author: Ankam Ravi Kumar
+#Version: 1.0
+#Modified Date:
+#Modified by:
+
+# START
+echo "Welcome $USER"
+echo "Your present working directory is `pwd`"
+echo "Today date is `date`"
+# END
