0694fdb5574c0980334d89739d75753b388ffec9
Ankam Ravi Kumar <techtutorials@users.noreply.github.com>
2018-05-17 23:17:21 +0530

    Create helloworld.sh

---
+++ helloworld.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Purpose: This is the Sample template File
+#Version: 1.0
+#Created Date: Thu May 3 11:55:43 IST 2018
+#Modified Date:
+#Author: Ankam Ravi Kumar
+# START #
+echo "Testing template file"
+# END #
