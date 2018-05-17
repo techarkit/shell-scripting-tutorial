5093a4bb415ee93bfa93117f09920db8f819ada2
Ankam Ravi Kumar <techtutorials@users.noreply.github.com>
2018-05-17 22:57:24 +0530

    Create countargs.sh
    Counting given positional parameters.

---
+++ countargs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Purpose: Counting given postional parameters.
+#Version:1.0
+#Created Date: Mon May 7 21:55:05 IST 2018
+#Modified Date:
+#Author: Ankam Ravi Kumar
+# START #
+#echo "Your current given parameters are $#"
+if [ $# -lt 1 ];then
+echo "Program Usage is './scriptname.sh' options"
+else
+echo "Program executed successfully"
+fi
+
+# END #
