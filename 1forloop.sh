81834e98eea399b95e67eb10a2291cd6601ff471
Ankam Ravi Kumar <techtutorials@users.noreply.github.com>
2018-05-17 23:16:51 +0530

    Create 1forloop.sh

---
+++ 1forloop.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#Purpose: One more example for for loop
+#Version:
+#Created Date: Wed May 16 19:31:50 IST 2018
+#Modified Date:
+#Author: Ankam Ravi Kumar
+# START #
+for i in 1 2 3 4 5
+do
+echo $i
+done
+# END #
