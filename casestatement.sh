070be29155a96e8f903b7b884c8b7bce387a8df9
Ankam Ravi Kumar <techtutorials@users.noreply.github.com>
2018-06-16 17:44:36 +0530

    Create casestatement.sh

---
+++ casestatement.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#Purpose: Example for Case Statement
+#Version:1.0
+#WebSite: https://arkit.co.in
+#Created Date: Mon May 21 20:37:59 IST 2018
+#Modified Date:
+#Author: Ankam Ravi Kumar
+# START #
+echo -c "Enter a number: \c"
+read -r a
+echo -c "Enter b number: \c"
+read -r b
+
+echo "1. Sum of values"
+echo "2. Substraction"
+echo "3. Multiplication"
+echo "4. Division"
+echo "5. Modulo division"
+echo -c "Enter Your Choice from above menu: \c"
+read -r ch
+case $ch in
+1) echo "Sum of $a + $b = "`expr $a + $b`;;
+2) echo "Subsctraction = "`expr $a - $b`;;
+3) echo "Multiplication = "`expr $a \* $b`;;
+4) echo "Division = "`expr $a / $b`;;
+5) echo "Modulo Division = "`expr $a % $b`;;
+*) echo "Invalid Option provided"
+esac
+# END #
