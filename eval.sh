909a5569d56f422d510acd037894879237546ee8
Ankam Ravi Kumar <techtutorials@users.noreply.github.com>
2018-06-16 17:50:58 +0530

    Create eval.sh
    eval command Evaluating twice

---
+++ eval.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#Purpose: eval command Evaluating twice
+#Version:1.0
+#Created Date: Wed Jun 13 22:09:59 IST 2018
+#Modified Date:
+#WebSite: https://arkit.co.in
+#Author: Ankam Ravi Kumar
+# START #
+COMMAND="ls -ltr /etc"
+echo "$COMMAND"
+eval $COMMAND
+# END #
