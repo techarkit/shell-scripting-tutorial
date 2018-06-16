54bb5123148a18b11b27fc61e6ff5c9a8cada68b
Ankam Ravi Kumar <techtutorials@users.noreply.github.com>
2018-06-16 17:47:49 +0530

    Create array.sh

---
+++ array.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#Purpose: Array Example
+#Version:1.0
+#Created Date: Mon May 28 22:59:22 IST 2018
+#Modified Date:
+#WebSite: https://arkit.co.in
+#Author: Ankam Ravi Kumar
+# START #
+fruits=( "Apple" "Orange" "Banana" "Sapota" )
+fruits[3]='Green Apple'
+for fruit in ${fruits[@]}
+do
+        echo "Fruit Name is $fruit"
+done
+
+echo "Number of Fruits in Bucket is" ${#fruits[@]}
+echo "All Fruits ${fruits[@]}"
+
+# END #
