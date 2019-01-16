a4c06e5e9058e1280ef60e18cb048ed357fcd092
Ankam Ravi Kumar <techtutorials@users.noreply.github.com>
2019-01-16 21:59:38 +0530

    Create userexists.sh

---
+++ userexists.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+##Purpose: Check given user Exits Or Not 
+##Date: 27th Oct 2016
+##Author: Ankam Ravi Kumar
+##WebSite: https://arkit.co.in
+
+##Start
+echo "Please Enter User name you want check:\c"
+read user
+grep $user /etc/passwd > /dev/null
+if [ $? -eq 0 ]; then
+grep $user /etc/passwd 
+echo "$user Exists in this Machine"
+else
+echo "$user does not exists"
+fi
+
+##END
