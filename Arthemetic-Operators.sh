079f038e96130376a4bb09e533ed508a27ebe8f9
T-u-s-h-a-r <68273051+T-u-s-h-a-r@users.noreply.github.com>
2020-10-01 00:54:09 +0530

    Update Arthemetic-Operators.sh

--- Arthemetic-Operators.sh
+++ Arthemetic-Operators.sh
@@ -10,11 +10,11 @@ read -r a
 echo -e "Please enter another value: \c"
 read -r b
 
-echo "a+b value is $(($a+$b))"
-echo "a-b value is $(($a-$b))"
-echo "axb value is $(($a*$b))"
-echo "a/b value is $(($a/$b))"
-echo "a%b value is $(($a%$b))"
+echo "a+b value is $(($a+$b))"    #it will add both the values
+echo "a-b value is $(($a-$b))"    #it will subtract b form a 
+echo "axb value is $(($a*$b))"    #it will multiply both a and b
+echo "a/b value is $(($a/$b))"    #it will divide b from a
+echo "a%b value is $(($a%$b))"    #it will give the remainder when a is divided by b
 
 echo "Completed successfully"
 
