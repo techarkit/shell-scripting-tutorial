92439bf1ed3de0dc4d09e526a948b50a746e39f6
FastFingertips <bugra.coskun@outlook.com>
2021-12-22 07:37:25 +0300

    Update while-loop.sh

--- while-loop.sh
+++ while-loop.sh
@@ -1,8 +1,10 @@
-While Loop Example, print any given number table.
-
 #!/bin/bash
-## While Loop Example with 2 table
-echo -e "Please provide one value:\c"
+# While Loop Example with 2 table, print any given number table.
+# See Full Explanation of this above shell script [while loop](https://www.youtube.com/Techarkit?sub_confirmation=1)
+
+#START
+
+echo -e "Please provide one value: \c"
 read -r c
 i=1
 while [ $i -le 10 ]
@@ -12,4 +14,4 @@ echo "$c * $i = $b"
 i=`expr $i + 1`
 done
 
-See Full Explanation of this above shell script [while loop](https://www.youtube.com/Techarkit?sub_confirmation=1)
+#END
